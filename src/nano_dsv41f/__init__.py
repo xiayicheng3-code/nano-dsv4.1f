9a80f303ad9128e56921099cb970d18a0d22fe79
xiayicheng3-code <xiayicheng3@gmail.com>
2026-09-13 18:48:53 -0400

    Export reference pretraining helpers

--- src/nano_dsv41f/__init__.py
+++ src/nano_dsv41f/__init__.py
@@ -18,7 +18,14 @@ from .config import (
 from .dspark import apply_dspark, sample_markov_block
 from .model import apply_model, apply_model_dspark, build_layer_specs, init_model
 from .optimizer import init_optimizer_state, optimizer_step, parameter_rule_map
-from .training import build_indexer_groups, selective_indexer_distillation_loss
+from .training import (
+    build_indexer_groups,
+    causal_lm_loss,
+    indexer_phase_enabled,
+    pretrain_loss,
+    pretrain_step,
+    selective_indexer_distillation_loss,
+)
 
 __all__ = [
     "AttentionConfig",
@@ -39,10 +46,14 @@ __all__ = [
     "apply_model_dspark",
     "build_indexer_groups",
     "build_layer_specs",
+    "causal_lm_loss",
+    "indexer_phase_enabled",
     "init_model",
     "init_optimizer_state",
     "optimizer_step",
     "parameter_rule_map",
+    "pretrain_loss",
+    "pretrain_step",
     "sample_markov_block",
     "selective_indexer_distillation_loss",
 ]
