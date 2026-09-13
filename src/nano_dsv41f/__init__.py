7512b8ffda63b961a4d777189e49f057ca930053
xiayicheng3-code <xiayicheng3@gmail.com>
2026-09-13 16:31:11 -0400

    Export nano model composition configs

--- src/nano_dsv41f/__init__.py
+++ src/nano_dsv41f/__init__.py
@@ -1,10 +1,23 @@
 """nano-dsv4.1f educational reference package."""
 
-from .config import AttentionConfig, IndexerTrainingConfig, ModelConfig, TrainConfig
+from .config import (
+    AttentionConfig,
+    CSA2Config,
+    EngramConfig,
+    IndexerTrainingConfig,
+    ModelConfig,
+    TrainConfig,
+)
+from .model import apply_model, build_layer_specs, init_model
 
 __all__ = [
     "AttentionConfig",
+    "CSA2Config",
+    "EngramConfig",
     "IndexerTrainingConfig",
     "ModelConfig",
     "TrainConfig",
+    "apply_model",
+    "build_layer_specs",
+    "init_model",
 ]
