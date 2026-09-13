0c1b57c492d70e2c58ec6acfc5fb43a45bfc16eb
xiayicheng3-code <xiayicheng3@gmail.com>
2026-09-13 15:58:45 -0400

    Initialize Python package

---
+++ src/nano_dsv41f/__init__.py
@@ -0,0 +1,10 @@
+"""nano-dsv4.1f educational reference package."""
+
+from .config import AttentionConfig, IndexerTrainingConfig, ModelConfig, TrainConfig
+
+__all__ = [
+    "AttentionConfig",
+    "IndexerTrainingConfig",
+    "ModelConfig",
+    "TrainConfig",
+]
